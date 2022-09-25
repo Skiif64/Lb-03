8c8a64d5b72761e526549c9cd7f5c61303fab98a
Skiif64 <86207504+Skiif64@users.noreply.github.com>
2022-09-25 21:25:34 +0400

    Добавьте файлы проекта.

---
+++ Lb-03/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Lb_03
+{
+    internal class Program
+    {
+        private const string FILE_PATH = @"\file.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите искомое слово:");
+            var word = Console.ReadLine();
+            WriteString(1000, word);
+            var text = ReadText();
+            var rkSearcher = new RabinKarpSearcher();
+            var sw = new Stopwatch();
+            Console.WriteLine($"Исходная строка: {text}");
+            sw.Start();
+            var result = rkSearcher.Search(text, word);
+            sw.Stop();
+            Console.WriteLine($"Алгоритм Рабина-Карпа. Результат: {result}. Отработано за {sw.Elapsed.TotalMilliseconds} мс.");
+            var kmpSearcher = new KnuthMorrisPrattSearcher();
+            sw.Restart();
+            var result2 = kmpSearcher.Search(text, word);
+            Console.WriteLine($"Алгоритм Кнута Морриса Пратта. Результат: {result2}. Отработано за {sw.Elapsed.TotalMilliseconds} мс.");
+
+            Console.ReadLine();
+        }
+
+        static void WriteString(int symbolCount, string barrierWord)
+        {
+            var random = new Random();
+            var str = "";
+            for (int i = 0; i < symbolCount; i++)
+            {
+                var symbolNumber = random.Next(1040, 1071);
+                var symbol = (char)symbolNumber;
+                str += symbol;
+            }
+            str += barrierWord;
+
+            using (var sw = new StreamWriter(FILE_PATH, false))
+            {
+                sw.WriteLine(str);
+            }
+        }
+
+        static string ReadText()
+        {
+            var str = "";
+            using (var sr = new StreamReader(FILE_PATH))
+            {
+                str = sr.ReadToEnd();
+            }
+
+            return str;
+        }
+    }
+}
---
+++ Lb-03/RabinKarpSearcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Lb_03
+{
+    public class RabinKarpSearcher
+    {
+        public string Search(string text, string word)
+        {
+            int n = text.Length;
+            int m = word.Length;
+            int hashText = Hash(text);
+            int hashWord = Hash(word);
+
+            string currentSegment = text.Substring(0, m);
+            for (int i = 0; i < n - m; i++)
+            {
+                if (hashText == hashWord)
+                    if (currentSegment == word)
+                        return currentSegment;
+
+                currentSegment = text.Substring(i + 1, m);
+                hashText = Hash(currentSegment);
+            }
+
+            return string.Empty;
+
+        }
+
+        private int Hash(string text)
+        {            
+            return text.GetHashCode();
+        }
+    }
+}
